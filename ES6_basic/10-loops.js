25a8b1fb1b983e721c1484bac5a58f1b35495add
natewood2 <nathanwood.sfg@gmail.com>
2024-05-02 09:44:07 -0500

    Adding 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const tempArray = [];
+  for (const temp of array) {
+    tempArray.push(appendString + temp)
+  }
+
+  return array;
+}
