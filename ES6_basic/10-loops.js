9d800dc7c1ba84e92d93df05d3ebee519b2e8a5f
natewood2 <nathanwood.sfg@gmail.com>
2024-05-02 09:46:12 -0500

    Forgot to change return statement

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const tempArray = [];
   for (const temp of array) {
-    tempArray.push(appendString + temp)
+    tempArray.push(appendString + temp);
   }
 
   return tempArray;
