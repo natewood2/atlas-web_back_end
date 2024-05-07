9d451268bffd13ea47642e90da6bd705d3021da6
natewood2 <nathanwood.sfg@gmail.com>
2024-05-07 17:30:30 -0500

    Adding 1

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+function getListStudentIds(getstudents) {
+  if (!Array.isArray(getstudents)) {
+    return [];
+  }
+
+  return getstudents.map(getstudents => getstudents.id);
+}
+
+export default getListStudentIds;
