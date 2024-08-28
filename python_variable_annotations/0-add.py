22e1523a045a0af2393f93e259479b6f48218457
natewood2 <nathanwood.sfg@gmail.com>
2024-08-28 16:09:25 -0500

    Correct lack of documentation.

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
+    """
+        Arguments: A and B. They return a float when given floats.
+        Return: Returns sum of A and B.
+    """
     return a + b
