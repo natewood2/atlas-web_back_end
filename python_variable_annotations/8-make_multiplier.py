ff270b91ebe60ae190ff9f778ceddd099e202489
natewood2 <nathanwood.sfg@gmail.com>
2024-08-29 11:46:38 -0500

    Adding number 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,6 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    def multiplier(x: float) -> float:
-        return x * multiplier
-    return multiplier
+    def multiply(num: float) -> float:
+        return num * multiplier
+    return multiply
