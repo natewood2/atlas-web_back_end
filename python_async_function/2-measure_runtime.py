adc16adbfc86530ea2c64734279c5a01d909e77f
natewood2 <nathanwood.sfg@gmail.com>
2024-08-30 14:36:56 -0500

    Adding number 2.

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,11 @@
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines.py').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    start = time.time()
+    await wait_n(n, max_delay)
+    end = time.time()
+    total = end - start
+    return total / n
