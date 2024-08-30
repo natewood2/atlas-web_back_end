1bff146ca2f7d9b1bc36c14f5e99a90784fddcc7
natewood2 <nathanwood.sfg@gmail.com>
2024-08-30 14:39:35 -0500

    Testing with build in Python executor. Forgot Shebang

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 import time
-wait_n = __import__('1-concurrent_coroutines.py').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
