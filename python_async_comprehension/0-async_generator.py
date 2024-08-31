d96fe1d1becac5119ae650a0aeab9cda116a4320
natewood2 <nathanwood.sfg@gmail.com>
2024-08-31 12:19:43 -0500

    Adding the first question with correction to the issues of no annotitions

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     This will loop 10 times, waiting 1 second then yield a random number.
     """
