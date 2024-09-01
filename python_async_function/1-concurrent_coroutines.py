55724fd7a4424f5fb3fec533e2d502b4331735d4
natewood2 <nathanwood.sfg@gmail.com>
2024-09-01 13:18:08 -0500

    Adding number 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
+"""
+this way on importing is terrible
+"""
 import asyncio
 from typing import List
 random_import = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    gross. really gross. dont even know what is going on
+    """
     tasks = [random_import(max_delay) for _ in range(n)]
 
     delays = await asyncio.gather(*tasks)
