85dd419ae8c4418ad4eefe7945592646fb34e086
natewood2 <nathanwood.sfg@gmail.com>
2024-09-01 12:50:40 -0500

    Adding 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""
+Gotta document!
+"""
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    adding documentation to the muplier
+    """
     def multiply(num: float) -> float:
         return num * multiplier
     return multiply
