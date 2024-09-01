325416b43b59d4b34940096723a022d2bbe498ec
natewood2 <nathanwood.sfg@gmail.com>
2024-09-01 12:35:23 -0500

    Adding 5-sum

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""
+importing list from typing, yeah that module
+"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    practicing documentation is this way is annoying but I understand why it is important
+    """
     return sum(input_list)
