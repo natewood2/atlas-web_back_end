159e4ebbc3c931d5cd1e234c66065ed81896c809
natewood2 <nathanwood.sfg@gmail.com>
2024-09-01 12:42:34 -0500

    Adding 7 and its documentation

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+"""
+Remember Tuples?
+"""
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    A pretty interesting function.
+    A lot going on but basically returns it both as a squared object
+    """
     return (k, v**2)
