fb9050559149b6eeaef07a24e123d4df8081ab32
natewood2 <nathanwood.sfg@gmail.com>
2024-09-01 13:37:19 -0500

    Adding documentation to number 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""
+more and more importing but the same stuff?
+"""
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    a little confusing but makes sense. to create a task to wait a random
+    about of time
+    """
     return asyncio.create_task(wait_random(max_delay))
