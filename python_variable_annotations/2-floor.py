11feb8a6d4f18ce39b9219b18b83c38bfc07562c
natewood2 <nathanwood.sfg@gmail.com>
2024-09-01 11:21:39 -0500

    Adding documentation to floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""
+importing math to find the floor
+"""
 import math
 
 
 def floor(n: float) -> int:
+    """
+    find the floor but return int not float
+    """
     return math.floor(n)
