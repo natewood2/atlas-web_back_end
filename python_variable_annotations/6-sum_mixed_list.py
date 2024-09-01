48c51b4a269dd668d950fe5abfbc1de81ef3c4d6
natewood2 <nathanwood.sfg@gmail.com>
2024-09-01 12:40:08 -0500

    Adding 6 and its documentation

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""
+importing List and Union to create stuff
+"""
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Documenting is awesome and this returns a sum of a list of a float
+    """
     return sum(mxd_lst)
