ac2667964b33f986fc7f61e334da11a2ef9d983f
natewood2 <nathanwood.sfg@gmail.com>
2024-09-01 21:55:27 -0500

    Adding 4 a change

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,7 +4,7 @@ still upset about how to import
 """
 import asyncio
 from typing import List
-task_wait_random = __import__('2-measure_runtime').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
