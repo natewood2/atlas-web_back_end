263ad996a788970c14357edbd033932166734e9d
natewood2 <nathanwood.sfg@gmail.com>
2024-09-13 18:07:09 -0500

    Password chcek works correctly now

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -12,3 +12,11 @@ def hash_password(password: str) -> bytes:
     encrypted_password = bcrypt.hashpw(password.encode('utf-8'),
                                        bcrypt.gensalt())
     return encrypted_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate that the provided password matches the hashed password
+    using BOOL
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
