1f8fadccd2c221b5c681de6f8b558b8975af5ac0
natewood2 <nathanwood.sfg@gmail.com>
2024-09-13 17:18:11 -0500

    Adding second verison of the Filtered Logger file

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,7 +5,27 @@ the specified fields within the log line.
 """
 import re
 from typing import List
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        initialize, so fields can be redacted
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        old_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, old_message, self.SEPARATOR)
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
