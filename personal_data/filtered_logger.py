a6baa5199db7efe4d88e67acdc9d91ae848d2b4b
natewood2 <nathanwood.sfg@gmail.com>
2024-09-13 17:01:17 -0500

    Adding person_data again

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,12 @@ Importing re for to obfuscate the values of
 the specified fields within the log line.
 """
 import re
+from typing import List
 
-def filter_datum(fields, redaction, message, separator):
-    return re.sub(rf'({separator})({"|".join(fields)}){separator}[^;]*', lambda m: f'{m.group(1)}{m.group(2)}{separator}{redaction}', message)
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    A Logging function the obfuscates log message
+    """
+    return re.sub(rf'({separator})({"|".join(fields)}){separator}[^;]*',
+                  lambda m: f'{m.group(1)}{m.group(2)}{separator}{redaction}',
+                  message)
