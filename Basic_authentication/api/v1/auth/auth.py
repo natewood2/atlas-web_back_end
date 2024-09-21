60241439a5354d985480b9ee8fc885f56f4d8214
natewood2 <nathanwood.sfg@gmail.com>
2024-09-21 16:38:08 -0500

    Adding the finished authorization header and will finishthe rest of problem 5 later

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,9 @@ class Auth:
         """
         Gets the authorization header from the request.
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
