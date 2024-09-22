c692437f4e9e4f1a8fa8fc6bd0131e8e2eea246d
natewood2 <nathanwood.sfg@gmail.com>
2024-09-21 21:08:40 -0500

    Adding number 9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic Authentication inheriting from
 Auth
 """
 from api.v1.auth.auth import Auth
+from typing import Tuple
 import base64
 
 
@@ -41,3 +42,19 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(self,
+                                decoded_base64_authorization_header:
+                                str) -> Tuple[str, str]:
+        """
+        Extracts credentials from the decoded base64
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return (email, password)
