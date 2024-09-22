ffadd85348e75775b070d83b77f77cdc4c1e7a4c
natewood2 <nathanwood.sfg@gmail.com>
2024-09-21 20:44:00 -0500

    Adding number 7 I believe, importing base64 and adding a method to decode it

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic Authentication inheriting from
 Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -24,3 +25,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split("Basic ", 1)[1]
+
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Decodes the Base64 strings
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
