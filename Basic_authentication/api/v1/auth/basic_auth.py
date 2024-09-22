136f94599532b84dc857ae3e9f317755f32ddcc9
natewood2 <nathanwood.sfg@gmail.com>
2024-09-21 20:19:45 -0500

    Adding basic auth, swithcing frombase 64

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,15 @@ class BasicAuth(Auth):
     """
     Managing API authentication
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extracts the base64 part of the Authorization
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        
+        return authorization_header.split("Basic ", 1)[1]
