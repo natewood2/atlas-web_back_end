f4aaa08935f6b6e99ffa2bbacc99f3c48e2e7b24
natewood2 <nathanwood.sfg@gmail.com>
2024-09-21 19:32:53 -0500

    Adding the finished version of number 5, I believe it is fully working

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,6 +14,32 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+auth = None
+auth_type = getenv("AUTH_TYPE")
+if auth_type:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """
+    Filter each request before processing.
+    """
+    if auth is None:
+        return
+    
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    
+    if auth.authorization_header(request) is None:
+        abort(401)
+    
+    if auth.current_user(request) is None:
+        abort(403)
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
