c93b3a4e48b050783c1daca19e2abbf830d26719
natewood2 <nathanwood.sfg@gmail.com>
2024-09-22 16:37:40 -0500

    Adding app.py changes

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -40,7 +40,7 @@ def before_request():
                       '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request)is None and auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
