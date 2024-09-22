ce3de7e7169381cd08e46075e84ae474adf22976
natewood2 <nathanwood.sfg@gmail.com>
2024-09-21 19:50:17 -0500

    Adding basic auth class

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication inheriting from
+Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Managing API authentication
+    """
+    pass
