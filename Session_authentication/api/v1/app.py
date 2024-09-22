93a927c72f9d8547dd9fcdf34bee637f3c8c3c7b
natewood2 <nathanwood.sfg@gmail.com>
2024-09-22 16:40:08 -0500

    Forgot to add auth_session inexculded path

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -37,7 +37,8 @@ def before_request():
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/vi/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request)is None and auth.session_cookie(request) is None:
