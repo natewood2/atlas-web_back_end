be3726e73aa3937ddce8f8f3642af8ac61fe8ea9
natewood2 <nathanwood.sfg@gmail.com>
2024-09-22 19:38:13 -0500

    Adding new handling

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -3,6 +3,8 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import User
@@ -40,3 +42,16 @@ class DB:
         self._session.commit()
         self._session.refresh(new_user)
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        finds a user via keyword arguments
+        """
+        session = self._session
+        try:
+            user = session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound(f"No user found for {kwargs}")
+        except InvalidRequestError:
+            raise InvalidRequestError(f"Invalid query arguments: {kwargs}")
