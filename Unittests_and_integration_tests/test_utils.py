d93faa25d09c2830af9fef14d9542ed86420da30
natewood2 <nathanwood.sfg@gmail.com>
2024-09-25 19:43:14 -0500

    Adding last for test utlis

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -23,15 +23,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, expected_key):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        
+
         self.assertIn(expected_key, str(context.exception))
+
 
 class TestGetJson(unittest.TestCase):
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-
     @patch('requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
         mock_response = Mock()
@@ -42,6 +42,7 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
 
 class TestMemoize(unittest.TestCase):
     def test_memoize(self):
