6ecf6b6dc8affcfe8efa4c065435db937cb6cf48
natewood2 <nathanwood.sfg@gmail.com>
2024-09-25 20:30:08 -0500

    Adding tests

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -15,3 +15,14 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         client.org()
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+    def test_public_repos_url(self):
+        payload = {"repos_url": "https://api.github.com/orgs/testorg/repos"}
+
+        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = payload
+            client = GithubOrgClient("testorg")
+            result = client._public_repos_url
+            
+            self.assertEqual(result, payload["repos_url"])
+            mock_org.assert_called_once()
