c0c98e9bd1ac017c4de86fb934b59fa726339237
natewood2 <nathanwood.sfg@gmail.com>
2024-09-25 19:25:58 -0500

    Addinging second phase of testing

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -11,3 +11,13 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "KeyError('a')"),
+        ({"a": 1}, ("a", "b"), "KeyError('b')"),
+    ])
+    def test_access_nested_map_exception(self, path, nested_map, expected_message):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        
+        self.assertEqual(str(context.exception), expected_message)
