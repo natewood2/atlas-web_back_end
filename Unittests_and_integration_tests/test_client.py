f75e149066425023d316d036d60f53e079f17d2b
natewood2 <nathanwood.sfg@gmail.com>
2024-09-25 19:54:27 -0500

    Adding test clients folder

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        client = GithubOrgClient(org_name)
+        client.org()
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
