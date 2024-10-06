7ab425a82ace194d3a9688698059357a8169ff93
natewood2 <nathanwood.sfg@gmail.com>
2024-10-05 20:28:15 -0500

    Adding the second question

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,9 +3,26 @@
 Creating a Cache class that store a Redis client
 through a private variable name
 """
+import functools
 import redis
 import uuid
 from typing import Union, Callable, Optional
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    decorator to count
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function 
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -19,6 +36,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores data in Redis and returns a key in string format
