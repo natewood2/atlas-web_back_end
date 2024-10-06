3b6f467d9dacfb40c885bec5baa2ca71d77087b5
natewood2 <nathanwood.sfg@gmail.com>
2024-10-05 21:39:07 -0500

    Adding updated SQL file

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create the users table only if it does not already exist
-CREATE TABLE IF NOT EXISTS users {
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-}
+)
