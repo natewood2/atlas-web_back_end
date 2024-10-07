f1c98f72abcdb7a5878a7f8b2766a452c2f74dd4
natewood2 <nathanwood.sfg@gmail.com>
2024-10-07 12:27:27 -0500

    Adding bands questoins

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Finding the best band, it's Blink-182 btw
-SELECT origin, SUM(total_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
