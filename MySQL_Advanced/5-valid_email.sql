dda2164c9f657fb88b08cae8ac7b202ce3297c9b
natewood2 <nathanwood.sfg@gmail.com>
2024-10-07 12:59:54 -0500

    Adding number 5

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Create another trigger, this one reset valid emails
+DELIMITER //
+
+CREATE TRIGGER reset_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END //
+
+DELIMITER ;
