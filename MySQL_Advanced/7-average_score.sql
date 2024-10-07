1504b28713d8c8da49068a86dcb8b58b9e6bb504
natewood2 <nathanwood.sfg@gmail.com>
2024-10-07 13:11:43 -0500

    Adding 7

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- I like sql
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = p_user_id;
+
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = p_user_id;
+    
+END //
+
+DELIMITER ;
