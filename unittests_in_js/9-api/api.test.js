21c83c325f39eac1e103639f79fc2b7d114f31ec
natewood2 <nathanwood.sfg@gmail.com>
2024-10-22 17:06:19 -0500

    Adding 9

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('should return status code 200 with correct message', (done) => {
+    request('http://localhost:7865/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('should return status code 200 for successful cart', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return status code 404', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
