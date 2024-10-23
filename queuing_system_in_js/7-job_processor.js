b0a3450d2582c24ea6735376193b9cd7fc2e1e3e
natewood2 <nathanwood.sfg@gmail.com>
2024-10-23 17:48:06 -0500

    Adding 7

---
+++ queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,31 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const bannedNumbers = ['4153518780', '4153518781'];
+
+/**
+ * @param {string} phoneNumber
+ * @param {string} message
+ * @param {object} job
+ * @param {function} done
+ */
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+
+  if (bannedNumbers.includes(phoneNumber)) {
+    return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  }
+
+  job.progress(50, 100);
+
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+  done();
+}
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
